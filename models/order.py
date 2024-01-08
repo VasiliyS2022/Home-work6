2bf406e223f01729a1751042e494cbaec4bb7023
VS <vskovrov@yandex.ru>
2024-01-08 15:17:01 +0300

    first commit

---
+++ models/order.py
@@ -0,0 +1,11 @@
+import datetime
+from pydantic import BaseModel, Field
+
+class OrderIn(BaseModel):
+    status: str
+
+class Order(BaseModel):
+    id: int
+    user_id: int
+    goods_id: int
+    order_date: str
